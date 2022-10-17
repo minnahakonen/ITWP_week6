80d253864e17e38ac51c393f4e40e2d04beea1d8
minnahakonen <101180642+minnahakonen@users.noreply.github.com>
2022-10-17 15:36:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -77,6 +77,7 @@ const getData = async () => {
 
   const res = await fetch(url, {
     method: "POST",
+    headers: {"content-type": "application/json"},
     body: JSON.stringify(jsonQuery)
   });
   if (!res.ok) {
@@ -126,6 +127,7 @@ const getUpdatedData = async () => {
 
   const res = await fetch(url, {
     method: "POST",
+    headers: {"content-type": "application/json"},
     body: JSON.stringify(newJsonQuery)
   });
   if (!res.ok) {
